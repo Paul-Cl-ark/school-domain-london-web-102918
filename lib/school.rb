f898e22b99b69d218c7521198562806d4a4dd68f
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-02 23:44:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+
+  @@all = []
+
+  attr_accessor :roster, :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+    @@all << self
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(class_num)
+    roster.map {|grade, students| students if grade == class_num}.compact.flatten # << code smell
+  end
+
+  def sort
+    sorted_hash = {}
+    roster.each {|grade, students| sorted_hash[grade] = students.sort}
+    sorted_hash
+  end
+
+  def self.all
+    @@all
+  end
+
+end
